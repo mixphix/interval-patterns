43d264feebb2621be6bdbce0bb3775c84af130fc
Melanie Brown <brown.m@pm.me>
2022-02-07 17:55:06 -0500

    OneOrTwo improvements and haddocks

--- src/Data/OneOrTwo.hs
+++ src/Data/OneOrTwo.hs
@@ -2,12 +2,28 @@ module Data.OneOrTwo where
 
 import Data.Data (Data)
 
+-- | Either one of something, or two of it.
+--
+-- Use 'oneOrTwo' to deconstruct.
 data OneOrTwo x
-  = One x
-  | Two x x
-  deriving (Eq, Ord, Show, Read, Generic, Data, Typeable)
+  = One !x
+  | Two !x !x
+  deriving
+    ( Eq,
+      Ord,
+      Show,
+      Read,
+      Generic,
+      Data,
+      Typeable,
+      Functor,
+      Foldable,
+      Traversable
+    )
 
+-- | Apply a 'oneOrTwo' argument function appropriately.
 oneOrTwo :: (x -> a) -> (x -> x -> a) -> OneOrTwo x -> a
 oneOrTwo f g = \case
   One x -> f x
   Two x y -> g x y
+{-# INLINE oneOrTwo #-}
