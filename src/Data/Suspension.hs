d1dce90e33d18c2eec29e464116271ccccb9c108
Melanie Brown <brown.m@pm.me>
2022-02-07 17:55:22 -0500

    Suspension improvements

--- src/Data/Suspension.hs
+++ src/Data/Suspension.hs
@@ -1,12 +1,35 @@
-module Data.Suspension where
+module Data.Suspension
+  ( Suspension (..),
+    suspension,
+    meridToMaybe,
+    fromMerid,
+    withMerid,
+    whenMerid,
+    meridToEither,
+  )
+where
 
-import Data.Data
+import Data.Data (Data)
 
+-- | The 'Suspension' of a type is its two-point extension:
+-- a "south pole" 'Nadir' and a "north pole" 'Zenit', if each value
+-- of the underlying type is considered as a 'Merid'ian.
 data Suspension a
   = Nadir
   | Merid a
   | Zenit
-  deriving (Eq, Ord, Show, Read, Generic, Data, Typeable, Functor)
+  deriving
+    ( Eq,
+      Ord,
+      Show,
+      Read,
+      Generic,
+      Data,
+      Typeable,
+      Functor,
+      Foldable,
+      Traversable
+    )
 
 instance Applicative Suspension where
   pure = Merid
@@ -30,14 +53,44 @@ instance (Semigroup x) => Semigroup (Suspension x) where
 instance (Semigroup x) => Monoid (Suspension x) where
   mempty = Nadir
 
+-- |
+-- @
+--   'suspension' nadir merid zenit = \\case
+--     Nadir -> nadir
+--     Merid a -> merid a
+--     Zenit -> zenit
+-- @
 suspension :: b -> (a -> b) -> b -> Suspension a -> b
 suspension nadir merid zenit = \case
   Nadir -> nadir
   Merid a -> merid a
   Zenit -> zenit
+{-# INLINE suspension #-}
 
-withMerid :: (a -> b) -> Suspension a -> Maybe b
-withMerid f = suspension Nothing (Just . f) Nothing
+-- | Unsafely extract the value from a 'Merid'.
+fromMerid :: HasCallStack => Suspension a -> a
+fromMerid = \case
+  Nadir -> error "expected Merid but got Nadir!"
+  Merid a -> a
+  Zenit -> error "expected Merid but got Zenit!"
 
+-- | Safely extract the value from a 'Merid'.
+meridToMaybe :: Suspension a -> Maybe a
+meridToMaybe = withMerid id
+{-# INLINE meridToMaybe #-}
+
+-- | Safely extract the value from a 'Merid',
+-- providing in case of failure whether the argument was 'Zenit'.
+meridToEither :: Suspension a -> Either Bool a
+meridToEither = suspension (Left False) Right (Left True)
+{-# INLINE meridToEither #-}
+
+-- | Apply the function to the inner value, if it exists.
+withMerid :: (Alternative m) => (a -> b) -> Suspension a -> m b
+withMerid f = suspension empty (pure . f) empty
+{-# INLINE withMerid #-}
+
+-- | Execute the action using the inner value, if it exists.
 whenMerid :: (Alternative m) => Suspension a -> (a -> m ()) -> m ()
 whenMerid s f = suspension pass f pass s
+{-# INLINE whenMerid #-}
