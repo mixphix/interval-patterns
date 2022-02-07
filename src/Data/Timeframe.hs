423a88f1c410ff455897202c9916891ccd8cc2bc
Melanie Brown <brown.m@pm.me>
2022-02-07 12:32:04 -0500

    localTimeframe, localTimeframeAt

--- src/Data/Timeframe.hs
+++ src/Data/Timeframe.hs
@@ -5,5 +5,12 @@ import Data.Time.Compat
 
 type Timeframe = Interval UTCTime
 
+localTimeframeAt :: TimeZone -> LocalTime -> LocalTime -> Timeframe
+localTimeframeAt = on (:||:) . localTimeToUTC
+
+localTimeframe :: LocalTime -> LocalTime -> IO Timeframe
+localTimeframe t1 t2 =
+  getCurrentTimeZone <&> \tz -> localTimeframeAt tz t1 t2
+
 duration :: Timeframe -> Maybe NominalDiffTime
 duration = measuring diffUTCTime
